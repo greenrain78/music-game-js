4ae490d4870b3545ad125bdffe0ac1ed221948c0
GreenRain <47217640+greenrain78@users.noreply.github.com>
2022-06-21 14:50:28 +0000

    anime 완성

--- vite-project/sample/game_test/v002/component/music_node/node_controller.js
+++ vite-project/sample/game_test/v002/component/music_node/node_controller.js
@@ -1,4 +1,6 @@
 import { setState } from "../../core/observer";
+// import { anime } from "../../lib/anime";
+import { MusicNode } from "./music_node";
 
 
 // music node를 생성, 관리, 소멸을 담당하는 객체
@@ -6,12 +8,13 @@ export class NodeController {
   
   constructor() {
     // 변수 할당
-
+    this.node_list = [];
     // 메인 엘리먼트 생성
     this.elements = document.createElement('div');
     this.elements.setAttribute("class", `node_controller`);
     // 하위 엘리먼트 생성
     this.createElement();
+    this.create_node('a', 1, 5, 3);
 
   }
 
@@ -23,7 +26,7 @@ export class NodeController {
     // 옵저버 키 설정
     this.setKeyState = setState(keyStateName);
   }
-
+ 
   createElement()  {
     // // 하위 엘리먼트 생성
     // const key_word = document.createElement('span');
@@ -34,12 +37,32 @@ export class NodeController {
     // this.elements.appendChild(style);
     // this.elements.appendChild(key_word);
   }
-  create_node(key, start_time, end_time, click_time){
-    const music_node = document.createElement('span');
-    music_node.textContent = this.word;
-    
-    this.elements.appendChild(music_node);
 
+  create_node(key, start_time, end_time, click_time){
+    // 초기 설정
+    const music_node_info = {};
+    music_node_info['node'] = new MusicNode(key, click_time);
+    music_node_info['start_time'] = start_time;
+    music_node_info['end_time'] = end_time * 1000;
+
+    // 객체 추가
+    this.elements.appendChild(music_node_info.node.elements);
+    // 에니메이션 동작
+    anime({
+      targets: music_node_info['node'].elements,
+      translateY: [ '0%', '200%' ],
+      scale: 2,
+      duration: end_time * 1000,
+      easing: 'easeInCirc',
+
+      // 종료시 에니메이션 설정
+      complete: function(anim) {
+        
+        console.log(anim);
+        music_node_info.node.deleteElement();
+        delete music_node_info.node   // 객체 제거
+      },
+    });
   }
   
 }
--- vite-project/sample/game_test/v002/component/world/world.js
+++ vite-project/sample/game_test/v002/component/world/world.js
@@ -16,46 +16,22 @@ class GameWorld extends HTMLElement {
     // Always call super first in constructor
     super();
     // 오브젝트 생성
-    this.test_board = new TestBoard();
     this.node_controller = new NodeController();
     // 엘리먼트 생성
     this.shadow = this.attachShadow({mode: 'open'});
     this.createElement();
     // 스타일 생성
     this.setStyle();
-    // 이벤트 등록
-    this.eventAction();
   }
-  eventAction(){
-    for (var keyName of keyStateList) {
-      subscribe(keyName, this.render.bind(this, keyName));
-    }
-   
-
-  }
-  render(keyName) {
-    
-    const keyState = getState(keyName); //Model의 상태를 가져와서 렌더링
-    const text1 = this.game_world.getElementsByTagName('h1')[0];
-    text1.textContent = `${keyName} => ${keyState}`;
-    this.test_board.reaction(keyName, keyState)
-
-  }
-
-
-
-  
+ 
   createElement()  {
 
     this.game_world = document.createElement('div');
     this.game_world.setAttribute("class", "game_world");
     
-    const text1 = document.createElement('h1');
-    text1.textContent = "게임 화면";
-    this.game_world.appendChild(text1)
-    this.game_world.appendChild(this.test_board.elements)
-    
-    
+
+    this.game_world.appendChild(this.node_controller.elements)
+        
     this.shadow.appendChild(this.game_world);
   }
 
@@ -66,12 +42,17 @@ class GameWorld extends HTMLElement {
   }
   setStyle(){
         // Apply external styles to the shadow dom
-    const linkElem = document.createElement('link');
-    linkElem.setAttribute('rel', 'stylesheet');
-    linkElem.setAttribute('href', 'component/world/world.css');
+    const world_css = document.createElement('link');
+    world_css.setAttribute('rel', 'stylesheet');
+    world_css.setAttribute('href', 'component/world/world.css');
+
+    const node_css = document.createElement('link');
+    node_css.setAttribute('rel', 'stylesheet');
+    node_css.setAttribute('href', 'component/music_node/node.css');
 
     // Attach the created elements to the shadow dom
-    this.shadow.appendChild(linkElem);
+    this.shadow.appendChild(world_css);
+    this.shadow.appendChild(node_css);
   }
 }
 
